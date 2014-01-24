17f4c1e37d72ea3e70a8a21a010e5aaa4b3bea88
Simon Spannagel <simon.spannagel@desy.de>
2014-01-24 13:26:51 +0100

    Add flash file for SW 1.17.

--- FLASH/makeimg.sh
+++ FLASH/makeimg.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+echo "Generating SOF flash file..."
 sof2flash --input="../dtb/dtb.sof" --output="dtb.flash" --epcs
+echo "Generating ELF flash file..."
 elf2flash --input="../software/dtb_expert/dtb_expert.elf" --output="dtb_expert.flash" --epcs --after="dtb.flash"
-cat dtb.flash dtb_expert.flash > dtb_v1.16.flash
+echo "Merging flash files..."
+cat dtb.flash dtb_expert.flash > dtb_v1.17.flash
 rm dtb.flash
 rm dtb_expert.flash
+echo "Done."
