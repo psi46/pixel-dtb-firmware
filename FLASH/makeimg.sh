bb482256363577cbc49e56670fdadc3fcabdf2df
Simon Spannagel <simon.spannagel@desy.de>
2014-01-28 18:21:11 +0100

    Fetch the software version from dtb_config.h and name the flash file after it.

--- FLASH/makeimg.sh
+++ FLASH/makeimg.sh
@@ -4,8 +4,10 @@ echo "Generating SOF flash file..."
 sof2flash --input="../dtb/dtb.sof" --output="dtb.flash" --epcs
 echo "Generating ELF flash file..."
 elf2flash --input="../software/dtb_expert/dtb_expert.elf" --output="dtb_expert.flash" --epcs --after="dtb.flash"
-echo "Merging flash files..."
-cat dtb.flash dtb_expert.flash > dtb_v1.17.flash
+echo "Fetching SW version..."
+version=$(grep "sw_version" ../software/dtb_expert/dtb_config.h | tr -dc '[:digit:]' | sed 's/^\(.\{1\}\)/\1./')
+echo "Merging flash files to dtb_v${version}.flash..."
+cat dtb.flash dtb_expert.flash > dtb_v${version}.flash
 rm dtb.flash
 rm dtb_expert.flash
 echo "Done."
