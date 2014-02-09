7ce4df30aa4425e66b1e729bebd09d2321c1cc90
Simon Spannagel <simon.spannagel@desy.de>
2014-02-09 11:59:58 +0100

    Bump version number

--- software/dtb_expert/dtb_config.h
+++ software/dtb_expert/dtb_config.h
@@ -9,7 +9,7 @@ using namespace std;
 #define VERSION(ver,subver) 256*ver+subver
 
 #define fw_version VERSION(1,10)  // Quartus design version
-#define sw_version VERSION(1,17)  // NIOS II software version
+#define sw_version VERSION(1,25)  // NIOS II software version
 
 
 struct DTB_CONFIG
