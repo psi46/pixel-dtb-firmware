0dec63609ce820dfe81f5413dacedeb47e33a948
Simon Spannagel <simon.spannagel@desy.de>
2014-07-04 13:39:46 +0200

    Add new Firmware version 3.1 featuring sparse DAC scans in 1D and 2D

--- software/dtb_expert/dtb_config.h
+++ software/dtb_expert/dtb_config.h
@@ -9,7 +9,7 @@ using namespace std;
 #define VERSION(ver,subver) 256*ver+subver
 
 #define fw_version VERSION(3,0)  // Quartus design version
-#define sw_version VERSION(3,0)  // NIOS II software version
+#define sw_version VERSION(3,1)  // NIOS II software version
 
 
 struct DTB_CONFIG
